39594d239a58310eabe745ea6ab9d09d512737d6
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-01-28 19:51:02 +0600

    Tokenizing words and Sentences

---
+++ 01. Tokenizing words and Sentences/main.py
@@ -0,0 +1,9 @@
+﻿from nltk import sent_tokenize, word_tokenize
+
+nltk_wiki = "The Natural Language Toolkit, or more commonly NLTK, is a suite of libraries and programs for symbolic and statistical natural language processing (NLP) for English written in the Python programming language. It was developed by Steven Bird and Edward Loper in the Department of Computer and Information Science at the University of Pennsylvania. NLTK includes graphical demonstrations and sample data. It is accompanied by a book that explains the underlying concepts behind the language processing tasks supported by the toolkit, plus a cookbook."
+
+for sentence in sent_tokenize(nltk_wiki):
+    print(sentence)
+
+for word in word_tokenize(nltk_wiki):
+    print(word)
