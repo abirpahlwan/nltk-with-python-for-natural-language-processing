b0c145243c98e70db53d44c487a34e0c7d9b52c1
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-02-01 11:41:30 +0600

    Stemming

---
+++ 03. Stemming/main.py
@@ -0,0 +1,18 @@
+﻿from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+
+def display(words):
+    for w in words:
+        print(ps.stem(w))
+
+
+# test_words = ['python', 'pythoner', 'pythoning', 'pythoned', 'pythonly']
+# display(test_words)
+
+test_sentence = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
+
+test_words = word_tokenize(test_sentence)
+display(test_words)
