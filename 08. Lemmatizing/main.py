ebe551d328f7d62db462bddcddd06f7490f02a24
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-02-01 14:25:02 +0600

    Lemmatizing

---
+++ 08. Lemmatizing/main.py
@@ -0,0 +1,15 @@
+﻿from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+
+def display(words):
+    for w in words:
+        print(lemmatizer.lemmatize(w))
+
+
+test_words = ['cats', 'geese', 'cacti', 'corpora']
+
+display(test_words)
+
+print(lemmatizer.lemmatize('better', pos='a'))
