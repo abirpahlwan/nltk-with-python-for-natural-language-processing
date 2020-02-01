5c58062f9ccbba2f701ec7ad11f12086aeb2c6ab
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-02-01 15:10:04 +0600

    NLTK Corpora

---
+++ 09. NLTK Corpora/main.py
@@ -0,0 +1,9 @@
+﻿from nltk.corpus import gutenberg
+from nltk.tokenize import sent_tokenize
+
+bible_text = gutenberg.raw('bible-kjv.txt')
+
+tokenized = sent_tokenize(bible_text)
+
+for s in tokenized[0:5]:
+    print(s)
