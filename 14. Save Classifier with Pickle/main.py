314ec3e4e20a278294cb7c211f0484cc1572d61b
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-04-04 07:40:44 +0600

    Save Classifier with Pickle

---
+++ 14. Save Classifier with Pickle/main.py
@@ -0,0 +1,48 @@
+﻿import nltk
+import random
+from nltk.corpus import movie_reviews
+import pickle
+
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
+
+all_words = []
+for word in movie_reviews.words():
+    all_words.append(word.lower())
+
+word_frequency = nltk.FreqDist(all_words)
+
+word_features = list(word_frequency.keys())[:3000]
+
+
+def find_features(document):
+    words = set(document)
+    features = {}  # Empty dictionary named 'features'
+    for word in word_features:
+        features[word] = (word in words)    # Check if 'word' exists in the 'words' set
+
+    return features
+
+
+# print(find_features(movie_reviews.words('neg/cv000_29416.txt')))
+
+feature_sets = [(find_features(review), category) for (review, category) in documents]
+
+training_set = feature_sets[:1900]
+testing_set = feature_sets[1900:]
+
+# classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+# classifier_file = open("naivebayes.pickle", "wb")
+# pickle.dump(classifier, classifier_file)
+# classifier_file.close()
+
+classifier_file = open("naivebayes.pickle", "rb")
+classifier = pickle.load(classifier_file)
+classifier_file.close()
+
+print("Naive Bayes Classifier accuracy: ", (nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
